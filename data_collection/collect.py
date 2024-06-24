68655dc0a1105d385ae7b3503efe209ec54d96ff
yono1 <yono@redhat.com>
2024-06-24 23:28:03 +0900

    commit

--- data_collection/collect.py
+++ data_collection/collect.py
@@ -3,11 +3,15 @@ from PIL import Image
 from io import BytesIO
 import logging
 import datetime
+import load_env
+from flask import Flask, render_template, Response
 
 # Loggerを初期化
 logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s",
                     level=logging.INFO)
 log = logging.getLogger(__name__)
+
+load_env.read_val_from_dotenv()
 
 def save_image_from_response(url, save_path):
     try:
@@ -22,6 +26,6 @@ def save_image_from_response(url, save_path):
         log.error("An error occurred:", str(e))
 
 if __name__ == "__main__":
-    url = "http://127.0.0.1:5000/save_image"
+    url = load_env.APP_SERVER + "/save_image"
     save_path = "images/" + datetime.datetime.now().strftime('%Y%m%d%H%M%S')+ "-image.jpg"
     save_image_from_response(url, save_path)
