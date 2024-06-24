e9a6b64393310be913c5266d294fa6320cdc8075
yono1 <yono@redhat.com>
2024-06-24 22:56:44 +0900

    commit

--- inference/scripts/ovms.sh
+++ inference/scripts/ovms.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-podman run --rm -u $(id -u) -v $(pwd)/models:/models -p 9000:9000 registry.connect.redhat.com/intel/openvino-model-server:latest --model_name yolov8n --model_path /models/yolov8n --port 9000 --shape auto
+podman run --rm -u $(id -u) -v /Users/yono/Documents/work/Edge-AI-for-video-demo/inference/src/models/yolov8n:/models/yolov8n -p 9000:9000 registry.connect.redhat.com/intel/openvino-model-server:latest --model_name yolov8n --model_path /models/yolov8n --port 9000 --shape auto
