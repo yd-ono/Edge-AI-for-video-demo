30bf2eeebf29b5e370512993667155e3f4699483
yono1 <yono@redhat.com>
2024-06-28 14:23:41 +0900

    commit

--- scripts/ovms/ovms-s3.sh
+++ scripts/ovms/ovms-s3.sh
@@ -4,7 +4,7 @@ podman run --rm -u $(id -u) \
 	-e AWS_ACCESS_KEY_ID="minio" \
 	-e AWS_SECRET_ACCESS_KEY="minio123" \
 	-e AWS_REGION="dummy" \
-	-e S3_ENDPOINT="http://s3-video-demo.apps.demo.sandbox2074.opentlc.com" \
+	-e S3_ENDPOINT="${S3_ENDPOINT}" \
 	-p 9000:9000 \
 	registry.connect.redhat.com/intel/openvino-model-server:latest \
 	--model_name yolov8n \
