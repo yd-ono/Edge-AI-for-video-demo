dce141f4d1067399d9229e912b997792f6d93833
yono1 <yono@redhat.com>
2024-07-03 11:41:20 +0900

    fix

--- scripts/ovms/ovms-s3.sh
+++ scripts/ovms/ovms-s3.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+NODE_IP=$1
+
 podman run --rm -u $(id -u) \
 	-e AWS_ACCESS_KEY_ID=minio \
 	-e AWS_SECRET_ACCESS_KEY=minio123 \
 	-e AWS_REGION=dummy \
-	-e S3_ENDPOINT=http://192.168.3.101:31306 \
+	-e S3_ENDPOINT=http://$NODE_IP:31306 \
 	-p 9000:9000 \
 	registry.connect.redhat.com/intel/openvino-model-server:latest \
 	--model_name demo \
