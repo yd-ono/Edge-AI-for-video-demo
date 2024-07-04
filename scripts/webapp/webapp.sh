6e94165845121cfac55352a53b96a7b2c77932a1
yono1 <yono@redhat.com>
2024-07-05 00:45:15 +0900

    commit

--- scripts/webapp/webapp.sh
+++ scripts/webapp/webapp.sh
@@ -7,6 +7,8 @@ podman run --rm -u $(id -u) \
 	-e MODEL_NAME=demo \
 	-e DEVICE="0" \
 	-e CONF="0.7" \
+	-e FPS="0.03" \
+	-e OVMS_TIMEOUT="0.03" \
 	-p 5555:5000 \
 	-v /dev:/dev \
 	quay.io/yono/yolov8-demo:1.0
