d14f1089647176a51dfee6d8f0f9a854448a1fa8
yono1 <yono@redhat.com>
2024-08-08 21:49:46 +0900

    commit

--- webapp/src/camera.py
+++ webapp/src/camera.py
@@ -3,15 +3,13 @@
 import cv2
 import load_env
 import time
+import threading
 
 load_env.read_val_from_dotenv()
 
 class Camera(object):
     def __init__(self):
         self.video = cv2.VideoCapture(load_env.DEVICE)
-
-    def __del__(self):
-        self.video.release()
 
     def __del__(self):
         self.video.release()
